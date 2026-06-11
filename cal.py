3e8bd5bf2b40f897cc012d0b735b661cf310cde4
thekashim <kashimkashim914@gmail.com>
2026-06-11 12:07:43 +0530

    initial commit :calculatore

---
+++ cal.py
@@ -0,0 +1,8 @@
+
+num1 =int(input("Enter the number :"))
+num2 =int(input("Enter the number :"))
+
+print("addtion",num1+num2)
+print("subtraction",num1-num2)
+print("multiplication",num1*num2)
+print("division",num1/num2)
